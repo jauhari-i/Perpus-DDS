838fdc8170b5ea7f9a282d5ac3241cf83e62fdee
jauhari-i <muhammad_irfan_27rpl@student.smktelkom-mlg.sch.id>
2020-03-03 15:12:57 +0700

    hello world

--- clients/src/config/Routes.js
+++ clients/src/config/Routes.js
@@ -3,20 +3,20 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { pages } from "../pages";
 
 const Routes = () => {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={pages.Home} />
-                <Route exact path="/peminjam" component={pages.Peminjams} />
-                <Route exact path="/petugas" component={pages.Petugases} />
-                <Route exact path="/anggota" component={pages.Anggotas} />
-                <Route exact path="/peminjam/:id" component={pages.Peminjam} />
-                <Route exact path="/petugas/:id" component={pages.Petugas} />
-                <Route exact path="/anggota/:id" component={pages.Anggota} />
-                <Route component={pages.Error404} />
-            </Switch>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={pages.Home} />
+        <Route exact path="/peminjam" component={pages.Peminjams} />
+        <Route exact path="/petugas" component={pages.Petugases} />
+        <Route exact path="/anggota" component={pages.Anggotas} />
+        <Route exact path="/peminjam/:id" component={pages.Peminjam} />
+        <Route exact path="/petugas/:id" component={pages.Petugas} />
+        <Route exact path="/anggota/:id" component={pages.Anggota} />
+        <Route component={pages.Error404} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default Routes;
