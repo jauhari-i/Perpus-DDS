c82a50cc03c869f1babc228a2d04875d247eaefd
Hammam Al-Hakim <hammam_al_27rpl@student.smktelkom-mlg.sch.id>
2020-03-04 06:50:11 +0700

    Add drawer list constant

---
+++ clients/src/constants/drawerListData.js
@@ -0,0 +1,17 @@
+const drawerListData = [
+  {
+    name: "Peminjaman",
+    icon: "Bookmark",
+    url: "/peminjaman"
+  },
+  {
+    name: "Anggota",
+    icon: "Group",
+    url: "/anggota"
+  },
+  {
+    name: "Petugas",
+    icon: "SupervisorAccount",
+    url: "/petugas"
+  }
+];
