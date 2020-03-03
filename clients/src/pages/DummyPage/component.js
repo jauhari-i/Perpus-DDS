d6ea4b8a9335d5a556ebd5750d9558353ad37a34
Hammam Al-Hakim <hammam_al_27rpl@student.smktelkom-mlg.sch.id>
2020-03-04 06:18:51 +0700

    Fix Drawer

--- clients/src/pages/DummyPage/component.js
+++ clients/src/pages/DummyPage/component.js
@@ -3,7 +3,12 @@ import Drawer from "../../components/element/Drawer";
 
 export class component extends Component {
   render() {
-    return <Drawer />;
+    return (
+      <Drawer>
+        basiowajdoi o wiod ahosk lakj hawoi dlao jeiof hanlksdn oaihd oshd
+        byawidb msuaiwf alwkdj wiod
+      </Drawer>
+    );
   }
 }
 
