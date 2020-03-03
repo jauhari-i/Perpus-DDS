c96211bc31a6cfc157e128496993ffd20e824489
Hammam Al-Hakim <hammam_al_27rpl@student.smktelkom-mlg.sch.id>
2020-03-03 13:32:48 +0700

    Test 2

--- clients/src/index.js
+++ clients/src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  <div>
-    <h1>Hello Babi</h1>
-  </div>,
-  document.getElementById("app")
+    <div>
+        <h1>Hello Babi</h1>
+        <h1>jforewugfhweo</h1>
+    </div>,
+    document.getElementById("app")
 );
