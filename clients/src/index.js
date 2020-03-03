fa6e4ae4446fa2330bf3140646212e735e4dcf77
jauhari-i <muhammad_irfan_27rpl@student.smktelkom-mlg.sch.id>
2020-03-03 13:48:03 +0700

    fix babel

--- clients/src/index.js
+++ clients/src/index.js
@@ -1,10 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-    <div>
-        <h1>Hello Babi</h1>
-        <h1>jforewugfhweo</h1>
-    </div>,
-    document.getElementById("app")
-);
+ReactDOM.render(<App />, document.getElementById("app"));
