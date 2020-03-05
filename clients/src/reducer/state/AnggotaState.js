c4fc69f7e12ee9613d859746f80a4d9037ee1db0
Hammam Al-Hakim <hammam_al_27rpl@student.smktelkom-mlg.sch.id>
2020-03-05 20:37:34 +0700

    Remove - unused import

--- clients/src/reducer/state/AnggotaState.js
+++ clients/src/reducer/state/AnggotaState.js
@@ -1,17 +1,16 @@
-import React, { useReducer } from "react"
-import axios from "../../config/axios-config"
-import firebase from "../../config/Firebase"
-import anggotaContext from "../context/anggotaContext"
-import anggotaReducer from "../reducer/anggotaReducer"
+import React, { useReducer } from "react";
+import firebase from "../../config/Firebase";
+import anggotaContext from "../context/anggotaContext";
+import anggotaReducer from "../reducer/anggotaReducer";
 import {
   DELETE_ANGGOTA,
   GET_ANGGOTA,
   ADD_ANGGOTA,
   EDIT_ANGGOTA
-} from "../types"
+} from "../types";
 
 const AnggotaState = props => {
-  const ref = firebase.firestore().collection("anggota")
+  const ref = firebase.firestore().collection("anggota");
   let anggota = [
     {
       kode_anggota: "AG0123",
@@ -37,34 +36,34 @@ const AnggotaState = props => {
       alamat: "Jl. Bunga Tulip",
       telepon: "081237492812"
     }
-  ]
+  ];
   const initialState = {
     data: []
-  }
+  };
 
-  const [state, dispatch] = useReducer(anggotaReducer, initialState)
+  const [state, dispatch] = useReducer(anggotaReducer, initialState);
 
   const deleteData = oldData => {
-    console.log(oldData)
+    console.log(oldData);
     ref
       .doc(oldData.kode_anggota)
       .delete()
       .then(res => console.log(res))
-      .catch(err => console.error(err))
-  }
+      .catch(err => console.error(err));
+  };
 
   const getAnggota = () => {
     ref.get().then(querySnapshot => {
-      const data = querySnapshot.docs.map(doc => doc.data())
+      const data = querySnapshot.docs.map(doc => doc.data());
       dispatch({
         type: GET_ANGGOTA,
         data: data
-      })
-    })
-  }
+      });
+    });
+  };
 
   const addAnggota = newData => {
-    let nData = newData
+    let nData = newData;
     ref
       .add({
         kode_anggota: nData.kode_anggota,
@@ -73,18 +72,18 @@ const AnggotaState = props => {
         telepon: nData.telepon
       })
       .then(() => getAnggota())
-      .catch(error => console.error("Error adding to database"))
-  }
+      .catch(error => console.error("Error adding to database"));
+  };
 
   const editAnggota = (newData, oldData) => {
-    let data = anggota
-    data[data.indexOf(oldData)] = newData
-    console.log(data.indexOf(newData))
+    let data = anggota;
+    data[data.indexOf(oldData)] = newData;
+    console.log(data.indexOf(newData));
     dispatch({
       type: EDIT_ANGGOTA,
       data: data
-    })
-  }
+    });
+  };
 
   return (
     <anggotaContext.Provider
@@ -98,7 +97,7 @@ const AnggotaState = props => {
     >
       {props.children}
     </anggotaContext.Provider>
-  )
-}
+  );
+};
 
-export default AnggotaState
+export default AnggotaState;
