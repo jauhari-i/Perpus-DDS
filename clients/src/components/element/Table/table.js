7b29fdfc683632443f8932c017d4006351837cc8
Hammam Al-Hakim <hammam_al_27rpl@student.smktelkom-mlg.sch.id>
2020-03-06 21:16:06 +0700

    Optimization and add new style

--- clients/src/components/element/Table/table.js
+++ clients/src/components/element/Table/table.js
@@ -1,7 +1,5 @@
 import React, { useState, useContext } from "react";
 import MaterialTable from "material-table";
-import PeopleAlt from "@material-ui/icons/PeopleAlt";
-import Button from "@material-ui/core/Button";
 import EditIcon from "@material-ui/icons/Edit";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
 import DeleteIcon from "@material-ui/icons/Delete";
@@ -10,12 +8,35 @@ import ClearIcon from "@material-ui/icons/Clear";
 import { green } from "@material-ui/core/colors";
 
 const table = props => {
+  let [selectedRow, setSelectedRow] = useState(null);
+
+  const optionConfig = {
+    pageSize: 10,
+    pageSizeOptions: [5, 10, 20, 30, 50, 75, 100],
+    actionsColumnIndex: -1,
+    rowStyle: rowData => ({
+      backgroundColor:
+        selectedRow && selectedRow.tableData.id === rowData.tableData.id
+          ? "#EEE"
+          : "#FFF"
+    })
+  };
+
+  const iconsConfig = {
+    Add: props => <AddCircleIcon color="primary" />,
+    Edit: props => <EditIcon style={{ color: green[500] }} />,
+    Delete: props => <DeleteIcon color="secondary" />,
+    Check: props => <CheckIcon style={{ color: green[500] }} />,
+    Clear: props => <ClearIcon color="secondary" />
+  };
+
   const _AddHandler = (newData, resolve) => {
     props.add(newData);
     setTimeout(() => {
       resolve();
     }, 2000);
   };
+
   const _EditHandler = (newData, oldData, resolve) => {
     if (oldData) {
       props.edit(newData, oldData);
@@ -24,12 +45,14 @@ const table = props => {
       resolve();
     }, 1500);
   };
+
   const _DeleteHandler = (oldData, resolve) => {
     props.delete(oldData);
     setTimeout(() => {
       resolve();
     }, 1500);
   };
+
   return (
     <MaterialTable
       title={props.title}
@@ -49,18 +72,11 @@ const table = props => {
             _DeleteHandler(oldData, resolve);
           })
       }}
-      options={{
-        pageSize: 10,
-        pageSizeOptions: [5, 10, 20, 30, 50, 75, 100],
-        actionsColumnIndex: -1
-      }}
-      icons={{
-        Add: props => <AddCircleIcon color="primary" />,
-        Edit: props => <EditIcon style={{ color: green[500] }} />,
-        Delete: props => <DeleteIcon color="secondary" />,
-        Check: props => <CheckIcon style={{ color: green[500] }} />,
-        Clear: props => <ClearIcon color="secondary" />
-      }}
+      onRowClick={(evt, selectedRow) =>
+        setSelectedRow((selectedRow = selectedRow))
+      }
+      options={optionConfig}
+      icons={iconsConfig}
     />
   );
 };
